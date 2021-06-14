2b0d33519e9b5a9d4cc576fa3152808ff6948aca
scratchpowered <85644596+scratchpowered@users.noreply.github.com>
2021-06-14 16:11:22 +0200

    Update config.testnet.ts

--- react-delegationdashboard/src/config.testnet.ts
+++ react-delegationdashboard/src/config.testnet.ts
@@ -37,7 +37,7 @@ export const network: NetworkType = {
   apiAddress: 'https://testnet-api.elrond.com',
   gatewayAddress: 'https://testnet-gateway.elrond.com',
   explorerAddress: 'http://testnet-explorer.elrond.com/',
-  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqp0llllswfeycs',
+  delegationContract: 'erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllllls27850s',
 };
 
 const networkSchema = object({
